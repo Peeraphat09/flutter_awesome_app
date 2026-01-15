af54a9060f3844e971661a070443fa576ece43a7
Peeraphat09 <werqazq48@gmail.com>
2026-01-15 16:13:25 +0700

    commit2: หน้า Home เสร็จแล้ว

---
+++ lib/views/home_ui.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+
+class HomeUi extends StatelessWidget {
+  const HomeUi({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.yellow,
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 100.0,
+            ),
+            Image.asset(
+              'assets/images/logo.png',
+              width: 300.0,
+              height: 300.0,
+              fit: BoxFit.cover,
+            ),
+            SizedBox(
+              height: 200.0,
+            ),
+            Text(
+              'ยินดีต้อนรับ DTI-SAU',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 28.0,
+                color: const Color.fromARGB(255, 0, 0, 0),
+              ),
+            ),
+            Text(
+              'Created by Peeraphat🥽🤣😫🧑🥞',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 18.0,
+                color: const Color.fromARGB(255, 0, 0, 0),
+              ),
+            ),
+            Text(
+              'Souteast Asia University',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 15.0,
+                color: const Color.fromARGB(255, 0, 0, 0),
+              ),
+            ),
+            SizedBox(
+              height: 50.0,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                OutlinedButton(
+                  onPressed: () {},
+                  child: Text(
+                    'LOGIN',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  style: OutlinedButton.styleFrom(
+                    fixedSize: Size(
+                      150.0,
+                      50.0,
+                    ),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 15.0,
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text(
+                    'SIGN UP',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    fixedSize: Size(
+                      150.0,
+                      50.0,
+                    ),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                    backgroundColor: Colors.black,
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
